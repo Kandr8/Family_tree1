874d1f4ecc90174d3242dec4988088ff15248794
kandr9 <kandr13579@gmail.com>
2024-06-04 00:07:22 +0300

    .

---
+++ src/Family__Tree1.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Family__Tree1 {
+//    Атрибуты древа: номер человека(счет),список,
+    private long humansID;
+    private List<Human> humanList;
+    public Family__Tree1(){
+        this()
+    }
+}
---
+++ src/Human.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class Human {
+//    Создаем атрибуты ли поля класса человек
+    String name;
+    Human mather, father;
+    List<Human> children;
+    LocalDate birthDate,deahtDate;
+    Gender gender;
+
+
+}
